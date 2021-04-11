34c5e5eed2511bc08a51b5c9c287400745c30ebd
Autofire <Autofire@users.noreply.github.com>
2021-04-11 10:53:38 -0700

    Fixed license headers.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 // This file is part of nfl2mac.
 // 
-// Foobar is free software: you can redistribute it and/or modify
+// nfl2mac is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 // 
-// Foobar is distributed in the hope that it will be useful,
+// nfl2mac is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
